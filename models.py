0261d5f3cf5697c872f4b5f6279b508bcea2fff2
nadavbennun1 <nadavbennun1@mail.tau.ac.il>
2024-07-28 15:45:50 +0300

    bug fixes + p0

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ import pandas as pd
 import scipy.stats as stats
 
 # Basic WF model: mutation, selection, drift
-def WF(s,mu,N, G, seed = 0):
+def WF(s,mu,N, G, seed = 0, p_init=0):
     # s: selection coefficient
     # mu: mutation rate
     # N: population size
@@ -14,10 +14,12 @@ def WF(s,mu,N, G, seed = 0):
     # Returns the frequency of the mutant allele at generation G
     np.random.seed(seed)
     p = np.zeros((G+1,2)) # array to store the frequency of the mutant allele
-    p[0] = np.array([1,0]) # initial frequency of the mutant allele
+    p[0] = np.array([1-p_init,p_init]) # initial frequency of the mutant allele
     w = 1+s # fitness of the mutant allele
-    M = np.array([[1-mu, 0], [mu, 1]]) # mutation matrix
-    S = np.array([[1, 0], [0, w]]) # selection matrix
+    M = np.array([[1-mu, 0], 
+                  [mu, 1]]) # mutation matrix
+    S = np.array([[1, 0], 
+                  [0, w]]) # selection matrix
     E = S@M # evolutionary matrix
     for i in range(G): # iterating over generations
         p[i+1] = E@p[i] # updating the frequency of the mutant allele
@@ -27,7 +29,7 @@ def WF(s,mu,N, G, seed = 0):
     return p
 
 # Wright-Fisher model with frequency-dependent selection
-def WF_FD(s,mu,N, G, seed = 0):
+def WF_FD(s, mu, N, G, seed = 0, p_init=0, alpha=7):
     # s: selection coefficient
     # mu: mutation rate
     # N: population size
@@ -35,11 +37,11 @@ def WF_FD(s,mu,N, G, seed = 0):
     # Returns the frequency of the mutant allele at generation G
     np.random.seed(seed)
     p = np.zeros((G+1,2)) # array to store the frequency of the mutant allele
-    p[0] = np.array([1,0]) # initial frequency of the mutant allele
+    p[0] = np.array([1-p_init,p_init]) # initial frequency of the mutant allele
     M = np.array([[1-mu, 0], [mu, 1]]) # mutation matrix
 
     for i in range(G): # iterating over generations
-        w = 1+s*(1-p[i,1]) # fitness of the mutant allele
+        w = 1+alpha*s*p[i,1]*(1-p[i,1]) # fitness of the mutant allele  
         S = np.array([[1, 0], [0, w]]) # selection matrix
         E = S@M # evolutionary matrix
         p[i+1] = E@p[i] # updating the frequency of the mutant allele
@@ -49,7 +51,7 @@ def WF_FD(s,mu,N, G, seed = 0):
     return p
 
 # Wright-Fisher model with bottleneck events
-def WF_bottleneck(s,mu,N, G, bottleneck, seed = 0):
+def WF_bottleneck(s, mu, N, G, bottleneck, p_init=0, seed = 0):
     # s: selection coefficient
     # mu: mutation rate
     # N: population size
@@ -59,7 +61,7 @@ def WF_bottleneck(s,mu,N, G, bottleneck, seed = 0):
     np.random.seed(seed)
     max_N = N
     p = np.zeros((G+1,2)) # array to store the frequency of the mutant allele
-    p[0] = np.array([1,0]) # initial frequency of the mutant allele
+    p[0] = np.array([1-p_init,p_init]) # initial frequency of the mutant allele
     M = np.array([[1-mu, 0], [mu, 1]]) # mutation matrix
     w = 1+s # fitness of the mutant allele
     S = np.array([[1, 0], [0, w]]) # selection matrix
@@ -77,7 +79,7 @@ def WF_bottleneck(s,mu,N, G, bottleneck, seed = 0):
     return p
 
 # Wright-Fisher model with exponential DFE
-def WF_DFE(s,mu,N, G, seed = 0):
+def WF_DFE(mu,N, G, dist, p_init=0, seed = 0):
     # s: selection coefficient
     # mu: mutation rate
     # N: population size
@@ -86,13 +88,10 @@ def WF_DFE(s,mu,N, G, seed = 0):
     np.random.seed(seed)
 
     # DFE
-    lower, upper = 0.5*s, 1.5*s
-    u, sigma = s, s/5
-    s_ = stats.truncnorm(
-        (lower - u) / sigma, (upper - u) / sigma, loc=u, scale=sigma).rvs(G)
+    s_ = dist.rvs(G)
 
     p = np.zeros((G+1,2)) # array to store the frequency of the mutant allele
-    p[0] = np.array([1,0]) # initial frequency of the mutant allele
+    p[0] = np.array([1-p_init,p_init]) # initial frequency of the mutant allele
     M = np.array([[1-mu, 0], [mu, 1]]) # mutation matrix
     for i in range(G): # iterating over generations
         w = 1+s_[i] # fitness of the mutant allele
