4728868ee4b27956c5e0163879850a3cd11f46d5
nadavbennun1 <nadavbennun1@mail.tau.ac.il>
2024-08-11 10:27:05 +0300

    other algorithms

--- inference.py
+++ inference.py
@@ -1,9 +1,5 @@
 # inference with NPE
 from models import WF
-import numpy as np
-from sbi import inference
-from sbi.inference import SNPE, prepare_for_sbi, simulate_for_sbi
-from sbi.utils import BoxUniform
 from sbi.inference.base import infer
 from inference_utils import get_prior
 import torch
@@ -22,8 +18,8 @@ def simulator(theta):
     return torch.tensor(res)
 
 # inference
-posterior = infer(simulator, prior, method='SNPE', num_simulations=10000)
+posterior = infer(simulator, prior, method='SNRE_A', num_simulations=10000)
 
 # Save the posterior with pickle
-with open('posterior.pkl', 'wb') as f:
+with open('posterior_bnre.pkl', 'wb') as f:
     pickle.dump(posterior, f)
